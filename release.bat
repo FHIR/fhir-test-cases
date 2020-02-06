78cc14660916987a4f962d7b784159245184a926
Grahame Grieve <grahameg@gmail.com>
2020-02-07 07:40:25 +1100

    * R5/snapshot-generation: Add a new test case for walking into a resource contained in a bundle * R5/snapshot-generation: add a new test case for walking into a slice in an extension * validator: Add a test case for search parameter type validation in CapabilityStatement

--- release.bat
+++ release.bat
@@ -11,7 +11,9 @@ echo ===========================================================================
 echo ..
 
 call mvn versions:set -DnewVersion=%newver%-SNAPSHOT
-call git commit -a -m "Release new version"
+
+call git commit -t v%newver% -a -m "Release new version %newver%"
+
 call git push origin master
 call "C:\tools\fnr.exe" -dir "C:\work\org.hl7.fhir\org.hl7.fhir.core" -fileMask "*.xml" -find "%oldver%-SNAPSHOT" -replace "%newver%-SNAPSHOT" -count 1
 call "C:\tools\fnr.exe" -dir "C:\work\org.hl7.fhir\latest-ig-publisher" -fileMask "*.html" -find "%oldver%" -replace "%newver%" -count 1
