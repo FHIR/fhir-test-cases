dafd02b590d5a131a2017defc8bd79264ec6158e
Grahame Grieve <grahameg@gmail.ccom>
2024-07-01 06:22:09 +0930

    add support for ndjson, and check that json object is finished when parsing + package versioning tests (WIP)

---
+++ validator/nested-package-version-dependencies/build-packages.sh
@@ -0,0 +1,23 @@
+cd hl7.fhir.test.versions#0.1.0
+tar -cvf hl7.fhir.test.versions#0.1.0.tar *
+cd ..
+gzip < hl7.fhir.test.versions#0.1.0/hl7.fhir.test.versions#0.1.0.tar > hl7.fhir.test.versions#0.1.0.tgz
+rm hl7.fhir.test.versions#0.1.0/hl7.fhir.test.versions#0.1.0.tar
+
+cd hl7.fhir.test.versions#0.2.0
+tar -cvf hl7.fhir.test.versions#0.2.0.tar *
+cd ..
+gzip < hl7.fhir.test.versions#0.2.0/hl7.fhir.test.versions#0.2.0.tar > hl7.fhir.test.versions#0.2.0.tgz
+rm hl7.fhir.test.versions#0.2.0/hl7.fhir.test.versions#0.2.0.tar
+
+cd hl7.fhir.test.versions.other#0.1.0
+tar -cvf hl7.fhir.test.versions.other#0.1.0.tar *
+cd ..
+gzip < hl7.fhir.test.versions.other#0.1.0/hl7.fhir.test.versions.other#0.1.0.tar > hl7.fhir.test.versions.other#0.1.0.tgz
+rm hl7.fhir.test.versions.other#0.1.0/hl7.fhir.test.versions.other#0.1.0.tar
+
+cd hl7.fhir.test.versions.other#0.2.0
+tar -cvf hl7.fhir.test.versions.other#0.2.0.tar *
+cd ..
+gzip < hl7.fhir.test.versions.other#0.2.0/hl7.fhir.test.versions.other#0.2.0.tar > hl7.fhir.test.versions.other#0.2.0.tgz
+rm hl7.fhir.test.versions.other#0.2.0/hl7.fhir.test.versions.other#0.2.0.tar
