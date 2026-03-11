933ce29fef5952e76d4236946d6e61f175b6fe77
Grahame Grieve <grahameg@gmail.ccom>
2026-03-11 12:10:14 +1100

    test cases to version dependency bugs

--- validator/packages/pack.sh
+++ validator/packages/pack.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-cd hl7.fhir.test.v1
-tar -czf hl7.fhir.test.v1.tgz package
+cd hl7.fhir.test.verA#1.0.0
+tar -czf hl7.fhir.test.verA#1.0.0.tgz package
 cd ..
-cp hl7.fhir.test.v1/hl7.fhir.test.v1.tgz hl7.fhir.test.v1.tgz
-rm hl7.fhir.test.v1/hl7.fhir.test.v1.tgz
+cp hl7.fhir.test.verA#1.0.0/hl7.fhir.test.verA#1.0.0.tgz hl7.fhir.test.verA#1.0.0.tgz
+rm hl7.fhir.test.verA#1.0.0/hl7.fhir.test.verA#1.0.0.tgz
 
-cd hl7.fhir.test.v2
-tar -czf hl7.fhir.test.v2.tgz package
+cd hl7.fhir.test.verA#2.0.0
+tar -czf hl7.fhir.test.verA#2.0.0.tgz package
 cd ..
-cp hl7.fhir.test.v2/hl7.fhir.test.v2.tgz hl7.fhir.test.v2.tgz
-rm hl7.fhir.test.v2/hl7.fhir.test.v2.tgz
+cp hl7.fhir.test.verA#2.0.0/hl7.fhir.test.verA#2.0.0.tgz hl7.fhir.test.verA#2.0.0.tgz
+rm hl7.fhir.test.verA#2.0.0/hl7.fhir.test.verA#2.0.0.tgz
 
-cd hl7.fhir.test.v3
-tar -czf hl7.fhir.test.v3.tgz package
+cd hl7.fhir.test.verB#1.0.0
+tar -czf hl7.fhir.test.verB#1.0.0.tgz package
 cd ..
-cp hl7.fhir.test.v3/hl7.fhir.test.v3.tgz hl7.fhir.test.v3.tgz
-rm hl7.fhir.test.v3/hl7.fhir.test.v3.tgz
+cp hl7.fhir.test.verB#1.0.0/hl7.fhir.test.verB#1.0.0.tgz hl7.fhir.test.verB#1.0.0.tgz
+rm hl7.fhir.test.verB#1.0.0/hl7.fhir.test.verB#1.0.0.tgz
+
+cd hl7.fhir.test.verB#2.0.0
+tar -czf hl7.fhir.test.verB#2.0.0.tgz package
+cd ..
+cp hl7.fhir.test.verB#2.0.0/hl7.fhir.test.verB#2.0.0.tgz hl7.fhir.test.verB#2.0.0.tgz
+rm hl7.fhir.test.verB#2.0.0/hl7.fhir.test.verB#2.0.0.tgz
+
+cd hl7.fhir.test.verC#1.0.0
+tar -czf hl7.fhir.test.verC#1.0.0.tgz package
+cd ..
+cp hl7.fhir.test.verC#1.0.0/hl7.fhir.test.verC#1.0.0.tgz hl7.fhir.test.verC#1.0.0.tgz
+rm hl7.fhir.test.verC#1.0.0/hl7.fhir.test.verC#1.0.0.tgz
